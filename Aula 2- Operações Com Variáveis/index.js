df5cfd3e60e90cb140dd8121843df90e80b575d2
ApenasEricles <ericlesprogrammer@gmail.com>
2022-10-15 17:43:32 -0300

    Treino

---
+++ Aula 2- Operações Com Variáveis/index.js
@@ -0,0 +1,37 @@
+//Valores das operações
+var resultado;
+var x = 5;
+var y = 2;
+
+resultado = x + y;
+console.log("Sua soma: ", resultado);
+//Resultado da soma
+
+resultado = x - y;
+console.log("Sua subtração: ", resultado);
+//Resultado da subtração
+
+resultado = x * y;
+console.log("Sua multiplicação: ", resultado);
+//Resultado da multiplicação
+
+resultado = x / y;
+console.log("Sua divisão: ", resultado);
+//Resultado da divisão
+
+resultado = x % y;
+console.log("Seu resto de divisão: ", resultado)
+
+console.log("----------------------------")
+
+//Calculo de IMC
+var peso = 60;
+var altura = 1.75
+var imc = (peso/(altura*altura))
+console.log("Seu imc é: ", imc)
+console.log('-----------------------------')
+
+//Somas de Strings
+var primeiroNome = 'Ericles'
+var segundoNome = 'Rangel'
+console.log('Nome Completo: ', primeiroNome +' '+ segundoNome)
---
+++ Aula 4- Type Metodos/String.js
@@ -0,0 +1,9 @@
+/*Métodos de Strings
+1 - Lenght
+2 - indexOf
+3 - search
+4 - Slice | Substring
+5 - Replace
+6 - UpperCase | LowerCase
+7 - Trim
+*/
